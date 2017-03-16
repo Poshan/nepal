2cea1d891712984f37a085ed2d3b1267ea824dbd
poshan <poshan.niraula@gmail.com>
2017-03-16 15:48:38 +0545

    removed jquery

--- js/map.js
+++ js/map.js
@@ -1,5 +1,6 @@
-$(document).ready(function () {
-  //console.log('inside the init');
+
+(function() {
+     //console.log('inside the init');
   var bounds = new L.latLngBounds(config["north_east"], config["south_west"]);
   map = L.map('map',{
     zoomControl:true
@@ -7,6 +8,5 @@ $(document).ready(function () {
   var mapbox = L.tileLayer(config["districtTiles"]);
   mapbox.addTo(map);
   // add the new control to the map
-  var zoomHome = new L.Control.zoomHome();
-  zoomHome.addTo(map);
-});
+
+})();
