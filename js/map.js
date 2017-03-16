cafe0e3efa21768e670b30b756771f8ce57c7009
poshan <poshan.niraula@gmail.com>
2017-03-16 15:53:36 +0545

    removed jquery

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@
 (function() {
      //console.log('inside the init');
   var bounds = new L.latLngBounds(config["north_east"], config["south_west"]);
-  map = L.map('map',{
+  map = L.map("map",{
     zoomControl:true
   }).setView(config["mapOptions"]["initialCenter"], config["mapOptions"]["zoomLevelMin"]);
   var mapbox = L.tileLayer(config["districtTiles"]);
