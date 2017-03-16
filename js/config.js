fd9fbff7d7834308a482484cecfb020b95434d88
poshan <poshan.niraula@gmail.com>
2017-03-16 13:58:52 +0545

    initail map load

---
+++ js/config.js
@@ -0,0 +1,12 @@
+config = {
+	"mapDOMId": "map",
+	"mapName": "Nepal Map",
+	"osmTiles": "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+	"mapOptions": {
+		"zoomLevelMin": 7,
+		"initialCenter": [28.35, 84.25]
+	},
+	"districtTiles": "https://{s}.tiles.mapbox.com/v4/poshan.dist-nepal/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicG9zaGFuIiwiYSI6InZ5a0dsLVkifQ.DGY81ha1q5VQpj3QA-REew",
+	"north_east": [26.328231, 80.029907],
+  	"south_west": [30.605155, 88.225708]
+  	}
---
+++ js/map.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  //console.log('inside the init');
+  var bounds = new L.latLngBounds(config["north_east"], config["south_west"]);
+  map = L.map(config["mapDOMId"],{
+    zoomControl:false,
+    measureControl: true
+  }).setView(config["mapOptions"]["initialCenter"], config["mapOptions"]["zoomLevelMin"]);
+  var mapbox = L.tileLayer(config["districtTiles"]);
+  mapbox.addTo(map);
+  // add the new control to the map
+  var zoomHome = new L.Control.zoomHome();
+  zoomHome.addTo(map);
+
+
+});
