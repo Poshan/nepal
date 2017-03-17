7fa2fc0d2da6e3f1a706a4d2d1adfbdb7ad36081
poshan <poshan.niraula@gmail.com>
2017-03-17 10:01:36 +0545

    added vdc tiles

--- js/config.js
+++ js/config.js
@@ -7,6 +7,7 @@ config = {
 		"initialCenter": [28.35, 84.25]
 	},
 	"districtTiles": "https://{s}.tiles.mapbox.com/v4/poshan.dist-nepal/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicG9zaGFuIiwiYSI6InZ5a0dsLVkifQ.DGY81ha1q5VQpj3QA-REew",
+	"vdcTiles": "https://{s}.tiles.mapbox.com/v4/poshan.vdc/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoicG9zaGFuIiwiYSI6InZ5a0dsLVkifQ.DGY81ha1q5VQpj3QA-REew",
 	"north_east": [26.328231, 80.029907],
   	"south_west": [30.605155, 88.225708]
   	}
